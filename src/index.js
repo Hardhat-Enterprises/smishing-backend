87d1ad91de2d3c9e2af4adc9cfe7f1a98809089e
Geraldo <Sugastinus@gmail.com>
2025-09-17 03:26:51 +1000

    feat(error): Global error handler

--- src/index.js
+++ src/index.js
@@ -9,7 +9,6 @@ import contactRoute from "./routes/contact.route.js";
 import securityMiddleware from "./middlewares/security.middleware.js";
 import { apiLimiter, authLimiter } from "./middlewares/RateLimiter.middleware.js";
 import whoisRoutes from "./routes/whois.route.js";
-
 import userRoute from "./routes/userUpdate.route.js";
 
 // calling body-parser to handle the Request Object from POST requests
@@ -25,7 +24,6 @@ app.use(apiLimiter);
 
 // Parse incoming JSON requests
 app.use(express.json());
-app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.urlencoded({ extended: true }));
 
 // Connect to MongoDB
@@ -48,6 +46,9 @@ app.use("/api/detections", detectionsRoute);
 
 // Mount update routes at /api/user
 app.use("/api/userUpdate", userRoute);
+
+// Apply error handler
+app.use(errorHandler);
 
 const PORT = process.env.PORT || 3000;
 
