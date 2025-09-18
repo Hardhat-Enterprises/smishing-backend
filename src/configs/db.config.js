f9e6a247e349308e95df924674f04b47ac453d7f
ananthkt20 <59090972+ananthkt20@users.noreply.github.com>
2025-09-18 16:44:46 +1000

    Update db.config.js

--- src/configs/db.config.js
+++ src/configs/db.config.js
@@ -1,17 +1,9 @@
 import mongoose from "mongoose";
-import dotenv from "dotenv";
-
-dotenv.config();
 
 async function connectDB() {
     try {
-        const uri = process.env.MONGO_URI; // using ENV variable instead of hardcoding
-        await mongoose.connect(uri, {
-            useNewUrlParser: true,
-            useUnifiedTopology: true,
-        });
-
-        console.log(" Connected to MongoDB");
+        await mongoose.connect(process.env.MONGODB_URI);
+        console.log("Connected to MongoDB");
     } catch (error) {
         console.error("MongoDB connection error:", error);
         process.exit(1); // Exit if database connection fails
