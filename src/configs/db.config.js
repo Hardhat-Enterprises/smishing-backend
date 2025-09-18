fae80c6352060d4768152a6b4d24b310461ec6cf
Ed-ward-Harris <68885764+Ed-ward-Harris@users.noreply.github.com>
2025-09-18 21:56:02 +1000

    Update db.config.js
    change mongoDB variable to correct name.

--- src/configs/db.config.js
+++ src/configs/db.config.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 async function connectDB() {
     try {
-        await mongoose.connect(process.env.MONGODB_URI);
+        await mongoose.connect(process.env.MONGO_URI);
         console.log("Connected to MongoDB");
     } catch (error) {
         console.error("MongoDB connection error:", error);
