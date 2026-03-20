24ed2fe5eb9095449f0f3a6e8c3765d0423a6ea4
Titiksha Rathod <s225090611@deakin.edu.au>
2026-03-20 16:31:12 +1100

    feat(security): add auth middleware and input validation to scan and spam routes

--- src/routes/spam.route.js
+++ src/routes/spam.route.js
@@ -1,9 +1,11 @@
-import express from 'express'
-import spamController from '../controllers/spam.controller.js';
+import express from "express";
+import spamController from "../controllers/spam.controller.js";
+import { authMiddleware } from "../middlewares/auth.middleware.js";
+import { validate, spamSchema } from "../utils/validation/auth.validation.js";
 
 const router = express.Router();
 
-// POST /textChecker
-router.post('/textChecker', spamController.checkSpamMessage)
+// POST /api/spam/textChecker
+router.post("/textChecker", authMiddleware, validate(spamSchema), spamController.checkSpamMessage);
 
 export default router;
