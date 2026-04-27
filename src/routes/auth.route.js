00ca2641268996154f2476d434ba3e8ec85ae47d
dil-thara <diltharanethmini@gmail.com>
2026-04-27 13:34:33 +1000

    Add login API hardening with sanitization and rate limiting

--- src/routes/auth.route.js
+++ src/routes/auth.route.js
@@ -1,5 +1,7 @@
 import express from "express";
 import { signup, verifyemail, login, forgotpassword, resetpassword } from "../controllers/auth.controller.js";
+import { sanitizeInput } from "../middlewares/sanitizeInput.js";
+import { loginLimiter } from "../middlewares/rateLimiter.js";
 
 const router = express.Router();
 
@@ -10,7 +12,8 @@ router.post("/signup", signup);
 router.post("/verify-email", verifyemail);
 
 // POST /login
-router.post("/login", login);
+// router.post("/login", login);
+router.post("/login", loginLimiter, sanitizeInput, login);
 
 // POST /forgot-password
 router.post("/forgot-password", forgotpassword);
