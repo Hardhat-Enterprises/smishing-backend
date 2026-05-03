566ce9cd23dbac56175e6422ba23791aa6981274
Niyati Udani <s225400403@deakin.edu.au>
2026-05-03 11:08:04 +1000

    feat(detections): add GET /api/detections route

--- src/routes/detections.route.js
+++ src/routes/detections.route.js
@@ -1,8 +1,11 @@
 import express from "express";
-import { exportCSV } from "../controllers/detections.controller.js";
+import { exportCSV, getDetectionsHandler } from "../controllers/detections.controller.js";
 
 const router = express.Router();
 
 router.get("/export", exportCSV);
+
+// New route to fetch detections with filtering and pagination
+router.get("/", getDetectionsHandler);
 
 export default router;
