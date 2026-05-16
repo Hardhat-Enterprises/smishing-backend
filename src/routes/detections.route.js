b512b8da2dfed75151cd72ffca0d4aac8a7f7f1c
Niyati Udani <s225400403@deakin.edu.au>
2026-05-17 09:53:04 +1000

    feat(detections): apply validation middleware and add summary route

--- src/routes/detections.route.js
+++ src/routes/detections.route.js
@@ -1,11 +1,15 @@
 import express from "express";
-import { exportCSV, getDetectionsHandler } from "../controllers/detections.controller.js";
+import { exportCSV, getDetectionsHandler, getDetectionsSummary } from "../controllers/detections.controller.js";
+import { validateDetectionQuery } from "../middlewares/validators/detection.validator.js";
 
 const router = express.Router();
 
 router.get("/export", exportCSV);
 
 // New route to fetch detections with filtering and pagination
-router.get("/", getDetectionsHandler);
+router.get("/", validateDetectionQuery, getDetectionsHandler);
+
+// New summary route
+router.get("/summary", getDetectionsSummary);
 
 export default router;
